9c2ffe0249f6660fcdb73cdd74f45ce0df7da33f
ZerAx <zerax2019@outlook.com>
2020-07-14 17:53:34 +0800

    added Entity logic

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+
+    private void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    /// <summary>
+    /// 伤害
+    /// </summary>
+    /// <param name="collision">碰撞的碰撞体</param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((gameObject.CompareTag("Enemy") && collision.CompareTag("Bullet0")) || (gameObject.CompareTag("Player") && collision.CompareTag("Bullet1")))
+        {
+            // 受伤
+            Hurt();
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void Hurt()
+    {
+        currentHP -= 1;
+        if (currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
