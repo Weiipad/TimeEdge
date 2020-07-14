0cbe5a7e3d04c5412809818950a5be12b984a54b
ZerAx <zerax2019@outlook.com>
2020-07-14 16:33:41 +0800

    Initial commit

---
+++ Assets/Scripts/BulletGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerator : MonoBehaviour
+{
+    public GameObject bullet;
+    public void Shoot()
+    {
+        Instantiate(bullet, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/GunBullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBullet : MonoBehaviour
+{
+    public BulletData data;
+
+    private new Rigidbody2D rigidbody;
+    private float timeAccumulator;
+
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = data.sprite;
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        rigidbody.velocity = data.velocity * Time.deltaTime * transform.up;
+        timeAccumulator += Time.deltaTime;
+        if (timeAccumulator >= data.duration)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
