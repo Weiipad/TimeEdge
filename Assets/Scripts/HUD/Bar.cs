5b0e5b4dd5707f7aeda58c9f795755de3a5f652b
学少何 <a664554724@qq.com>
2020-07-19 16:33:17 +0800

    split class \'Bar\' function

--- Assets/Scripts/HUD/Bar.cs
+++ Assets/Scripts/HUD/Bar.cs
@@ -3,16 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Bar : MonoBehaviour
+public abstract class Bar : MonoBehaviour
 {
-    public enum BarType
-    {
-        none,
-        blood,
-        shield,
-        loadBullet,
-    }
-
     public enum BarDirect
     {
         vertical,
@@ -20,147 +12,55 @@ public class Bar : MonoBehaviour
     }
 
     /// <summary>
-    /// 显示的类别
-    /// </summary>
-    public BarType barType = BarType.none;
-
-    /// <summary>
     /// 信息条的变化方向
     /// </summary>
     public BarDirect barDirect = BarDirect.horizontal;
-
-    public Color BloodBarColor = Color.red;
-    public Color ShieldBarColor = Color.blue;
-    public Color LoadBulletColor = Color.gray;
-
-    /// <summary>
-    /// 游戏物体标签，按照标签寻找物体
-    /// </summary>
-    public string EntityTag;
 
     /// <summary>
     /// 镜像显示
     /// </summary>
     public bool Mirror = false;
 
-    private GameEntity targetEntity;
-    private Weapon weapon;
-    private bool isGetEntity;
-
-    private RectTransform currentBar;
-
-    private Image childImage;
-
-    private float maxBarWidth = 0f;
-    private float maxBarHeight = 0f;
-
-    private void Start()
-    {
-        if (EntityTag != null)
-        {
-            targetEntity = GameObject.FindGameObjectWithTag(EntityTag).GetComponent<GameEntity>();
-            isGetEntity = true;
-        }
-
-        currentBar = transform.GetChild(0).GetComponent<RectTransform>();
-        if (Mirror)
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
-        }
-        else
-        {
-            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
-
-        }
-    
-        childImage = transform.GetChild(0).GetComponent<Image>();
-
-        var rectTransformSizeDelta = GetComponent<RectTransform>().sizeDelta;
-        if (rectTransformSizeDelta != Vector2.zero)
-        {
-            maxBarWidth = rectTransformSizeDelta.x;
-            maxBarHeight = rectTransformSizeDelta.y;
-            currentBar.sizeDelta = rectTransformSizeDelta;
-            currentBar.anchoredPosition = new Vector2(0f, -rectTransformSizeDelta.y);
-        }
-
-        switch(barType)
-        {
-            case BarType.blood      :childImage.color = BloodBarColor;break;
-            case BarType.shield     :childImage.color = ShieldBarColor;break;
-            case BarType.loadBullet :childImage.color = LoadBulletColor;break;
-            default:break;
-        }
-
-        if(barType == BarType.loadBullet && EntityTag != "Player")
-        {
-            throw new System.Exception("只有玩家才能使用武器装填信息条");
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        BarStatusUpdate();
-    }
-
     /// <summary>
-    /// 暂时无法获取武器数据，会出错
+    /// 开始时当前值是否为最大值
     /// </summary>
-    private void BarStatusUpdate()
-    {
-        if (barType == BarType.none || targetEntity == null || currentBar == null || maxBarWidth.Equals(0f))
-        {
-            if(targetEntity == null)
-            {
-                if (isGetEntity)
-                {
-                    Destroy(gameObject);
-                }
-            }
-            return;
-        }
-        float current;
-        float max;
-        if(barType == BarType.blood)
-        {
-            current = targetEntity.currentHP;
-            max = targetEntity.maxHP;
-        }
-        else if(barType == BarType.shield)
-        {
-            current = targetEntity.currentShield;
-            max = targetEntity.maxShield;
-        }
-        else if(barType == BarType.loadBullet)
-        {
-            if(weapon == null)
-            {
-                weapon = targetEntity.gameObject.GetComponent<Player>().GetWeapon;
-                if (weapon == null)
-                    return;
-            }
-            current = weapon.Load;
-            max = weapon.FullLoad;
-        }
-        else
-        {
-            return;
-        }
+    public bool IsMaxValueOnStart = true;
 
-        float percent = current / max;
-        if (current.Equals(0f))
-            percent = 0f;
-        if(percent.Equals(1f) || percent > 1f)
-            percent = 1f;
-        if (barDirect == BarDirect.horizontal)
+    protected RectTransform currentBar;
+
+    protected Image childImage;
+
+    protected float maxBarWidth = 0f;
+    protected float maxBarHeight = 0f;
+
+    protected float preValue;
+    protected float currentValue;
+    protected float maxValue;
+
+    protected abstract void BarInit();
+
+    protected abstract void BarStatusUpdate();
+    protected abstract float GetCurrentValue();
+    protected abstract void UpdateMaxValue();
+
+    public delegate void OnValueChangeDelegate();
+    public event OnValueChangeDelegate OnValueChangeEvent;
+
+    public void AddListener(OnValueChangeDelegate method)
+    {
+        OnValueChangeEvent += method;
+    }
+
+    public void DeleteListener(OnValueChangeDelegate method)
+    {
+        OnValueChangeEvent -= method;
+    }
+
+    protected void OnValueChange()
+    {
+        if (OnValueChangeEvent != null)
         {
-            float newWidth = maxBarWidth * percent;
-            currentBar.sizeDelta = new Vector2(newWidth, currentBar.sizeDelta.y);
-        }
-        else if (barDirect == BarDirect.vertical)
-        {
-            float newHeight = maxBarHeight * percent;
-            currentBar.sizeDelta = new Vector2(currentBar.sizeDelta.x, newHeight);
+            OnValueChangeEvent();
         }
     }
 }
---
+++ Assets/Scripts/HUD/GameEntityBar.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameEntityBar : Bar
+{
+    public enum BarType
+    {
+        none,
+        blood,
+        shield,
+        loadBullet,
+    }
+
+    /// <summary>
+    /// 显示的类别
+    /// </summary>
+    public BarType barType = BarType.none;
+
+    public Color BloodBarColor = Color.red;
+    public Color ShieldBarColor = Color.blue;
+    public Color LoadBulletColor = Color.gray;
+
+    /// <summary>
+    /// 游戏物体标签，按照标签寻找物体
+    /// </summary>
+    public string EntityTag;
+
+    private GameEntity targetEntity;
+    private Weapon weapon;
+    private bool isGetEntity;
+
+    private void Start()
+    {
+        BarInit();
+    }
+
+    private void FixedUpdate()
+    {
+        BarStatusUpdate();
+    }
+
+    protected override void BarInit()
+    {
+        currentBar = transform.GetChild(0).GetComponent<RectTransform>();
+        if (Mirror)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 180f));
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
+
+        }
+
+        childImage = transform.GetChild(0).GetComponent<Image>();
+
+        var rectTransformSizeDelta = GetComponent<RectTransform>().sizeDelta;
+        if (rectTransformSizeDelta != Vector2.zero)
+        {
+            maxBarWidth = rectTransformSizeDelta.x;
+            maxBarHeight = rectTransformSizeDelta.y;
+            currentBar.sizeDelta = rectTransformSizeDelta;
+            currentBar.anchoredPosition = new Vector2(0f, -rectTransformSizeDelta.y);
+        }
+
+        if (EntityTag != null)
+        {
+            targetEntity = GameObject.FindGameObjectWithTag(EntityTag).GetComponent<GameEntity>();
+            isGetEntity = true;
+        }
+
+        switch (barType)
+        {
+            case BarType.blood: childImage.color = BloodBarColor; break;
+            case BarType.shield: childImage.color = ShieldBarColor; break;
+            case BarType.loadBullet: childImage.color = LoadBulletColor; break;
+            default: break;
+        }
+
+        if (barType == BarType.loadBullet && EntityTag != "Player")
+        {
+            throw new System.Exception("只有玩家才能使用武器装填信息条");
+        }
+
+        UpdateMaxValue();
+
+        currentValue = GetCurrentValue();
+    }
+
+    protected override void BarStatusUpdate()
+    {
+        if (barType == BarType.none || targetEntity == null || currentBar == null || maxBarWidth.Equals(0f) || maxValue.Equals(0f))
+        {
+            if (targetEntity == null)
+            {
+                if (isGetEntity)
+                {
+                    Destroy(gameObject);
+                }
+            }
+            return;
+        }
+
+        UpdateMaxValue();
+        preValue = currentValue;
+        currentValue = GetCurrentValue();
+
+        if (!preValue.Equals(currentValue))
+        {
+            OnValueChange();
+        }
+        float percent = currentValue / maxValue;
+        if (currentValue.Equals(0f))
+            percent = 0f;
+        if (percent.Equals(1f) || percent > 1f)
+            percent = 1f;
+        if (barDirect == BarDirect.horizontal)
+        {
+            float newWidth = maxBarWidth * percent;
+            currentBar.sizeDelta = new Vector2(newWidth, currentBar.sizeDelta.y);
+        }
+        else if (barDirect == BarDirect.vertical)
+        {
+            float newHeight = maxBarHeight * percent;
+            currentBar.sizeDelta = new Vector2(currentBar.sizeDelta.x, newHeight);
+        }
+    }
+
+    protected override float GetCurrentValue()
+    {
+        float current = 0f;
+        if (barType == BarType.blood)
+            current = targetEntity.currentHP;
+        else if (barType == BarType.shield)
+            current = targetEntity.currentShield;
+        else if (barType == BarType.loadBullet)
+        {
+            if (weapon == null)
+            {
+                weapon = targetEntity.gameObject.GetComponent<Player>().GetWeapon;
+                if (weapon == null)
+                    return current;
+            }
+            current = weapon.Load;
+        }
+        return current;
+    }
+
+    protected override void UpdateMaxValue()
+    {
+        if(!isGetEntity)
+        {
+            BarInit();
+        }
+
+        if (barType == BarType.blood)
+            maxValue = targetEntity.maxHP;
+        else if (barType == BarType.shield)
+            maxValue = targetEntity.maxShield;
+        else if (barType == BarType.loadBullet)
+        {
+            if (weapon == null)
+            {
+                weapon = targetEntity.gameObject.GetComponent<Player>().GetWeapon;
+                if (weapon == null)
+                    return;
+            }
+            maxValue = weapon.FullLoad;
+        }
+    }
+}
