c7ee00b852b3a8580edad610d9a70eb1cc7bbd05
ZerAx <zerax2019@outlook.com>
2020-08-05 16:27:31 +0800

    merge

--- Assets/Scripts/GamePlay/Weapons/WeaponPos.cs
+++ Assets/Scripts/GamePlay/Weapons/WeaponPos.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class WeaponPos : MonoBehaviour
 {
     public Weapon weapon;
     private void Start()
     {
+        
         if (weapon != null)
             weapon.Equip(GetComponent<GameEntity>());
     }
