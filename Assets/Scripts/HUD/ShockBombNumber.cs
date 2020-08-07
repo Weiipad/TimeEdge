07316ecf43d8d25989b918b6b01e1f765d3b48d9
疯味十三君 <a664554724@qq.com>
2020-08-07 20:03:21 +0800

    add ShockBombNumebr HUD

---
+++ Assets/Scripts/HUD/ShockBombNumber.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShockBombNumber : MonoBehaviour
+{
+    private Player player;
+    private int preCount;
+    private int curCount;
+
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        text = gameObject.transform.GetChild(0).GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curCount = player.avaliableShock;
+        if(preCount != curCount)
+        {
+            preCount = curCount;
+            text.text = " x " + curCount.ToString();
+        }
+        else
+        {
+            if (text.text == null || text.text == "")
+            {
+                text.text = " x " + curCount.ToString();
+            }
+        }
+    }
+}
