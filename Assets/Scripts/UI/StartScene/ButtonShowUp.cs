46776e6b32ed65819e3d9bcd1bd1daa6b431072f
疯味十三君 <a664554724@qq.com>
2020-08-07 10:40:42 +0800

    remove TimeLine,change start scene button's way to show up

---
+++ Assets/Scripts/UI/StartScene/ButtonShowUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonShowUp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    new Animation animation;
+    Button button;
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+        button = GetComponent<Button>();
+        button.enabled = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animation["StartSceneButton"].speed = 1f;
+        animation.Play();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (animation.isPlaying)
+            animation["StartSceneButton"].time = animation["StartSceneButton"].time;
+        else
+            animation["StartSceneButton"].time = animation["StartSceneButton"].length;
+        animation["StartSceneButton"].speed = -1f;
+        animation.Play();
+        button.enabled = false;
+    }
+}
