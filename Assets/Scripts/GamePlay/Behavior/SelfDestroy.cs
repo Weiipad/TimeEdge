9714726d22061888fb3f9a528f572324bf115b1d
ZerAx <zerax2019@outlook.com>
2020-09-05 21:51:59 +0800

    pre-added some class

---
+++ Assets/Scripts/GamePlay/Behavior/SelfDestroy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroy : MonoBehaviour
+{
+    public float lifeDuration;
+
+    private float timeElapsed;
+    void Start()
+    {
+        timeElapsed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > lifeDuration) {
+            Destory(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/BulletGenerate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerate : MonoBehaviour
+{
+    public BulletSpawnMode mode;
+    public Bullet bullet;
+
+    public void OnDestory() {
+        mode.Ganerate(gameObject, bullet.gameObject);
+    }
+}
